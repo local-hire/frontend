836d7a3f155b590c1ee0290759d5550d584f7240
guymcmanusgithub <guy@guymcmanus.com>
2019-02-13 20:34:19 +1100

    added cateogies

--- src/components/categories/CategoryList.js
+++ src/components/categories/CategoryList.js
@@ -4,24 +4,34 @@ export default function CategoryList() {
   return (
     <div>
       <ul>
-        <li></li>
-        <li></li>
+        <li>Antiques and Art</li>
+        <li>Art and Crafts</li>
+        <li>Baby Essentials</li>
+        <li>Books</li>
+        <li>Business Supplies</li>
+        <li>Cameras</li>
+        <li>Cars, Bikes and Boats</li>
+        <li>Clothing</li>
+        <li>Computers, Tablets and Hardware</li>
+        <li>Electronics </li>
         <li>Garden Tools </li>
-        <li>Power Tools </li>
         <li>Gym and Sporting Equipment</li>
-        <li>Vehicles </li>
+        <li>Health and Beauty</li>
+        <li>Home Appliances and Accessories</li>
+        <li>Home and Garden</li>
+        <li>Home Entertainment</li>
+        <li>Industrial Equipment</li>
+        <li>Jewellery</li>
+        <li>Movies, Dvd's</li>
+        <li>Musical instruments and Equipment</li>
+        <li>Pet Supplies</li>
+        <li>Phones, Smart Watches, Fitbits, Accessories</li>
+        <li>Sporting Equipment</li>
+        <li>Power Tools </li>
         <li>Board Games</li>
         <li>Toys</li>
-        <li>Electronics </li>
-        <li>Video Games</li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
+        <li>Vehicle Parts and Accessories</li>
+        <li>Video Games and Consoles</li>
       </ul>
     </div>
   )
